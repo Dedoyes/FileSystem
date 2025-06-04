6ca015bde63ece68535d38fc484e3dd1effab097
Aby5s <lumia799506470@163.com>
2025-06-04 16:06:04 +0800

    fix lots of bug

--- storage.cpp
+++ storage.cpp
@@ -14,141 +14,41 @@ extern DiskIndexNodeCluster cluster;
 extern fileTree ft;
 extern UserCluster userGroup; 
 
-void storageSuperBlock () {
-    std::ofstream os (VDISK_START_FILE, std::ios::binary); 
-    if (!os) {
-        std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryOutputArchive superBlockArchive (os);
-    superBlockArchive (super);
-    os.close ();
-}
-
-void storageInodeForest () {
-    std::ofstream os (VDISK_START_FILE, std::ios::binary);
-    if (!os) {
-        std::cout << "Error : VIRTUAL START Disk can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryOutputArchive forestArchive (os);
-    forestArchive (forest);
-    os.close ();
-}
-
-void storageDiskIndexNodeCluster () {
+void storageAlldata (AllData &allData) {
     std::ofstream os (VDISK_START_FILE, std::ios::binary);
     if (!os) {
         std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
         exit (1);
     }
-    cereal::BinaryOutputArchive diskIndexNodeArchive (os);
-    diskIndexNodeArchive (cluster);
+    cereal::BinaryOutputArchive AllDataArchive (os);
+    AllDataArchive (allData);
     os.close ();
 }
 
-void storageFileTree () {
-    std::ofstream os (VDISK_START_FILE, std::ios::binary);
-    if (!os) {
-        std::cout << "Error : VIRTUAL START Disk can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryOutputArchive fileTreeArchive (os);
-    fileTreeArchive (ft);
-    os.close ();
-}
-
-void storageUserGroup () {
-    std::ofstream os (VDISK_START_FILE, std::ios::binary);
-    if (!os) {
-        std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryOutputArchive userArchive (os);
-    userArchive (userGroup);
-    os.close ();
-}
-
-void readSuperBlock () {
+void readAllData (AllData &allData) {
     std::ifstream is (VDISK_START_FILE, std::ios::binary);
     if (!is) {
         std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
         exit (1);
     }
-    cereal::BinaryInputArchive superBlockArchive (is);
-    superBlockArchive (super);
+    cereal::BinaryInputArchive AllDataArchive (is);
+    AllDataArchive (allData);
     is.close ();
-}
+} 
 
-void readInodeForest () {
-    std::ifstream is (VDISK_START_FILE, std::ios::binary);
-    if (!is) {
-        std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryInputArchive forestArchive (is);
-    try {
-        forestArchive(forest);
-    } catch (const std::exception& e) {
-        std::cout << "Deserialization error: " << e.what() << std::endl;
-        exit(1);
-    }    
-    std::cout << "vis size: " << forest.vis.size() << std::endl;
-    std::cout << "forest size: " << forest.forest.size() << std::endl;
-    forestArchive (forest);
-    is.close ();
-}
-
-void readDiskIndexNodeCluster () {
-    std::ifstream is (VDISK_START_FILE, std::ios::binary);
-    if (!is) {
-        std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryInputArchive clusterArchive (is);
-    clusterArchive (cluster);
-    is.close ();
-}
-
-void readFileTree () {
-    std::ifstream is (VDISK_START_FILE, std::ios::binary);
-    if (!is) {
-        std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryInputArchive fileTreeArchive (is);
-    fileTreeArchive (ft);
-    is.close ();
-}
-
-void readUserGroup () {
-    std::ifstream is (VDISK_START_FILE, std::ios::binary);
-    if (!is) {
-        std::cout << "Error : VIRTUAL START DISK can not be opened!" << std::endl;
-        exit (1);
-    }
-    cereal::BinaryInputArchive userGroupArchive (is);
-    userGroupArchive (userGroup);
-    is.close ();
+void storage () {
+    AllData alldata = AllData (super, forest, cluster, ft, userGroup);
+    storageAlldata (alldata);
 }
 
 void load () {
-    readSuperBlock ();
-    std::cout << "super load success." << std::endl;
-    readFileTree ();
-    std::cout << "file tree load success." << std::endl;
-    readInodeForest ();
-    std::cout << "inode forest load success." << std::endl; 
-    readUserGroup ();
-    std::cout << "user group load success." << std::endl;
-    readDiskIndexNodeCluster ();
-    std::cout << "cluster load success" << std::endl;
+    AllData allData = AllData (super, forest, cluster, ft, userGroup);
+    readAllData (allData);
+    super = allData.super;
+    forest = allData.forest;
+    cluster = allData.cluster;
+    ft = allData.ft;
+    userGroup = allData.userGroup;
 }
 
-void storage () {
-    storageSuperBlock ();
-    storageFileTree ();
-    storageInodeForest ();
-    storageUserGroup ();
-    storageDiskIndexNodeCluster ();
-}
+
--- storage.hpp
+++ storage.hpp
@@ -1,5 +1,34 @@
 #ifndef __STORAGE_HPP
 #define __STORAGE_HPP
+#include "SuperBlock.hpp"
+#include "inodeTree.hpp"
+#include "IndexNode.hpp"
+#include "fileTree.hpp"
+#include "user.hpp"
+
+class AllData {
+public :
+    SuperBlock super;
+    inodeForest forest;
+    DiskIndexNodeCluster cluster;
+    fileTree ft;
+    UserCluster userGroup;
+    AllData (SuperBlock& _super, inodeForest& _forest, DiskIndexNodeCluster& _cluster, 
+             fileTree& _ft, UserCluster& _userGroup) {
+        this->super = _super;
+        this->forest = _forest;
+        this->cluster = _cluster;
+        this->ft = _ft;
+        this->userGroup = _userGroup;
+    }
+    template <class Archive>
+    void serialize (Archive& ar) {
+        ar (super, forest, cluster, ft, userGroup);
+    }
+};
+
+void storageAlldata (AllData &allData);
+void loadAlldata (AllData &allData);
 
 void storageSuperBlock (void);
 void readSuperBlock (void);
